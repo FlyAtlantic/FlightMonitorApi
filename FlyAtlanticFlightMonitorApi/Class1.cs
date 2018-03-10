486308ba0e4e868a0d65aa79ed63ab0d8c202ba7
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-03-10 16:27:50 +0000

    Add project files.

---
+++ FlyAtlanticFlightMonitorApi/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyAtlanticFlightMonitorApi
+{
+    public class Class1
+    {
+    }
+}
